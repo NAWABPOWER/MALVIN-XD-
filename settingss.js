4b518f41659fbfe96d1991f0688f80e6af42baae
NAWABPOWER <tulinoobdi@gmail.com>
2025-08-16 06:14:02 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~lqUiCaZa#l_iTt7uYb4zQ8Y02533LIk-gnwc0Lt8N5QMUpCoRMSU",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "!", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "india/Harare" //put your country timeZone....leave blank if u don't know.
 };
